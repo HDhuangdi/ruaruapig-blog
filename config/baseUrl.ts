ed53d3a1b5b4897844e81326bbefb421deb06116
黄迪 <5668555+HDhuang_di@user.noreply.gitee.com>
2021-01-30 14:22:29 +0800

    none

--- config/baseUrl.ts
+++ config/baseUrl.ts
@@ -1,4 +1,4 @@
 const DEVELOPMENT = 'http://localhost:9966';
-const PRODUCTION = 'http://ruaruapig.xyz:1111';
+const PRODUCTION = 'http://ruaruapig.xyz:9966';
 
 export default { DEVELOPMENT, PRODUCTION };
